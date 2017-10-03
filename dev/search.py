9b5d4e4b7992f6a4ecbb785fc703aab61edf9661
Yujin Tang <lerrytang@gmail.com>
2017-10-03 11:08:57 +0900

    Shrink the search space

--- dev/search.py
+++ dev/search.py
@@ -42,44 +42,32 @@ if __name__ == "__main__":
 
     # parameters space to search
     ob_processors = ["norm1storder", "bodyspeed"]
-    mirror_obs = [True, False]
-    toe_dist_thresholds = [0.1, 0.2]
-    annealing_steps = [2000000, 4000000, 8000000, 16000000]
-    train_every_s = np.arange(5) + 1
-    actor_l2_actions = np.logspace(-6, -3, 5)
+    annealing_steps = [4000000, 8000000, 16000000]
+    lrelus = [0.0, 0.1, 0.3, 0.5]
     jumps = [False, True]
-    net_archs = [[128, 128, 64, 64],
-            [256, 256, 128, 64],
-            [512, 512, 128],
-            [1024, 512]]
+    use_bns = [False, True]
+    rs_delta_vels = [False, True]
 
     config = load_config("default.yaml")["DDPG"]
     config["agent"] = "DDPG"
 
     # workers
-    MAX_NUM_WORKERS = 8
+    MAX_NUM_WORKERS = 7
     worker_pool = Pool(processes=MAX_NUM_WORKERS)
 
     # grid search
-    for train_every in train_every_s:
-        for ob_processor in ob_processors:
-            for mirror_ob in mirror_obs:
-                for toe_dist_threshold in toe_dist_thresholds:
-                    for annealing_step in annealing_steps:
-                        for actor_l2_action in actor_l2_actions:
-                            for jump in jumps:
-                                for net_arch in net_archs:
-                                    p_config = deepcopy(config)
-                                    p_config["train_every"] = train_every
-                                    p_config["ob_processor"] = ob_processor
-                                    p_config["mirror_ob"] = mirror_ob
-                                    p_config["toe_dist_threshold"] = toe_dist_threshold
-                                    p_config["annealing_steps"] = annealing_step
-                                    p_config["actor_l2_action"] = actor_l2_action
-                                    p_config["jump"] = jump
-                                    p_config["actor_hiddens"] = net_arch
-                                    p_config["critic_hiddens"] = net_arch
-                                    worker_pool.apply_async(func=train, args=(p_config,))
+    for ob_processor in ob_processors:
+        for jump in jumps:
+            for annealing_step in annealing_steps:
+                for lrelu in lrelus:
+                    for use_bn in use_bns:
+                        p_config = deepcopy(config)
+                        p_config["ob_processor"] = ob_processor
+                        p_config["jump"] = jump
+                        p_config["annealing_steps"] = annealing_step
+                        p_config["lrelu"] = lrelu
+                        p_config["use_bn"] = use_bn
+                        worker_pool.apply_async(func=train, args=(p_config,))
 
     # clean up
     worker_pool.close()
